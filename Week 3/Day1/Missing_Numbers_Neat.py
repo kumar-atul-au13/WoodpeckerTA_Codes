e68e97820796992cdfd04dafe5aa735599c76749
kumar-atul-au7 <atultunnu2@gmail.com>
2020-04-29 23:37:10 +0530

    Create Missing_Numbers_Neat.py

---
+++ Week 3/Day1/Missing_Numbers_Neat.py
@@ -0,0 +1,14 @@
+def missingNumbers(arr,brr):
+    carr = [0]*101
+    minb=min(brr)
+    for b in brr:
+        carr[b-minb] += 1
+
+    for a in arr:
+        carr[a-minb] -= 1
+
+    return [indx+minb for indx,cnt in enumerate(carr) if cnt>0]
+
+arr=[int(x) for x in "203 204 205 206 207 208 203 204 205 206".split()]
+brr=[int(x) for x in "203 204 204 205 206 207 204 205 201 205 208 203 206 205 206 204".split()]
+print(missingNumbers( arr,brr))
