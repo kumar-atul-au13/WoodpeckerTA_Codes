5a6d07741234e081428bef770dd41fb2526e88e7
Kumar Atul <atultunnu2@gmail.com>
2020-05-05 17:02:51 +0530

    Ice Cream Parlor

--- Week 3/Day2/w3d2CC/CountSort/CountSort3.py
+++ Week 3/Day2/w3d2CC/CountSort/CountSort3.py
@@ -1,3 +1,6 @@
+n=int(input())
+arr=[ int(input().split(" ")[0]) for x in range(n)]
+
 def countingSort(arr):
     count_arr=[0]*100
     # [[0]*4 for i in range(4)]
@@ -5,6 +8,6 @@ def countingSort(arr):
         count_arr[elem]+=1
     for indx,elem in enumerate(count_arr[1:],1):
         count_arr[indx]+=count_arr[indx-1]
-
     return count_arr
+print(*countingSort(arr))
 countingSort([5,2,3,5,2,2,4,6,6,6,8,8])
