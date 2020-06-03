4095ddd85bb682f080530708330f10c76ebb1a8a
Kumar Atul <atultunnu2@gmail.com>
2020-06-03 19:41:14 +0530

    Kruskals

---
+++ Week7/Day5/Asignment/Prims.py
@@ -0,0 +1,30 @@
+graph = {
+    'A': {'B': 2, 'D': 4},
+    'B': {'C': 1, 'A': 2},
+    'D': {'A': 4, 'C': 3, 'E': 2, 'F': 11},
+    'E': {'D': 2, 'F': 7},
+    'F': {'D': 11, 'E': 7},
+    'C': {'B': 1, 'D': 3, 'H': 2, 'G': 6},
+    'G': {'C': 6, 'H': 2, 'I': 4},
+    'H': {'C': 2, 'G': 2, 'I': 1},
+    'I': {'G': 4, 'H': 1}
+}
+
+import heapq
+lst_for_hp=[] #prone
+# EDGE FORMAT x = ('a','b',3)  weight=x[3] starting_vertex = x[0] ending_vertex = x[1]
+
+heapq.heappush(lst_for_hp,(0,"H",None))
+infected_set=set()
+answer_edge=[]
+while len(lst_for_hp)>0:
+    poped_vertex = heapq.heappop(lst_for_hp)
+    if poped_vertex[1] not in infected_set:
+        infected_set.add(poped_vertex[1])
+        answer_edge.append(poped_vertex)
+    else:
+        continue
+    for adj_vertex, weight in graph[poped_vertex[1]].items():
+        if adj_vertex not in infected_set:
+            heapq.heappush(lst_for_hp,(weight,adj_vertex,poped_vertex[1]))
+print(answer_edge)
