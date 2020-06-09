7ca9982b58e48eccdc2bb6005617152a5eb9a285
Kumar Atul <atultunnu2@gmail.com>
2020-06-10 01:13:23 +0530

    all possible bst

--- Week7/Day4/CC/All_posible_bst.py
+++ Week7/Day4/CC/All_posible_bst.py
@@ -1,1 +1,17 @@
 #https://www.geeksforgeeks.org/construct-all-possible-bsts-for-keys-1-to-n/
+#Video: https://drive.google.com/file/d/1LdECD6ZcXG0HlKyY_p_NbkGx2udkM-DE/view?usp=sharing
+
+def all_possible_bst(start,end):
+    if start>end:
+        return [[]]
+    ans=[]
+    for root in range(start,end+1):#root-2
+        larr=all_possible_bst(start,root-1)#-- 3
+        rarr=all_possible_bst(root+1,end)#-- 4
+        for lcomb in larr:
+            lcomb.insert(0,root)
+            for rcomb in rarr:
+                ans.extend([lcomb+rcomb])# lcomb and rcomb are arrays
+    return ans
+print(all_possible_bst(1,4))
+
